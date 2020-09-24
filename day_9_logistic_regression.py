51792648e6923f7fac7f30ce432ef47f71b2b178
richjin518 <richjin518@gmail.com>
2020-09-24 10:52:17 -0700

    update model dump and load, PCA py rename

--- day_9_logistic_regression.py
+++ day_9_logistic_regression.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report
 from sklearn.metrics import roc_auc_score
+from sklearn.externals import joblib
 
 def cancer_tumor_demo():
     # 1) load data and add columns name
@@ -38,6 +39,77 @@ def cancer_tumor_demo():
     estimator = LogisticRegression()
     estimator.fit(x_train, y_train)
 
+    joblib.dump(estimator, 'D:\DL_data\lr.pkl')
+
+    # 6) model evaluation
+    print("weights: ", estimator.coef_)
+    print("b: ", estimator.intercept_)
+
+    y_predict = estimator.predict(x_test)
+    #print("Result compare: ", y_predict == y_test)
+    score = estimator.score(x_test, y_test)
+    #print ("accuracy is: ", score)
+
+    # precision, recall, F-1 score
+    report = classification_report(y_test, y_predict, labels=[2,4], target_names=["good", "bad"])
+    print(report)
+
+    # 5) estimator 2
+    estimator2 = joblib.load('D:\DL_data\lr.pkl')
+
+    # 6) model evaluation 2
+    print("weights: ", estimator2.coef_)
+    print("b: ", estimator2.intercept_)
+
+    y_predict = estimator2.predict(x_test)
+    #print("Result compare: ", y_predict == y_test)
+    score = estimator2.score(x_test, y_test)
+    #print ("accuracy is: ", score)
+
+    # precision, recall, F-1 score 2
+    report2 = classification_report(y_test, y_predict, labels=[2,4], target_names=["good", "bad"])
+    print(report2)
+
+    # ROC and AUC when sample not even'y distributed
+    # TPR = TP / (TP + FN) -> recall
+    # FPR = FP / (FP + TN)
+    # to calculate AUC 0 -> negative, 1 -> positive  
+    # AUC: [0.5 - 1.0]
+    y_true = np.where(y_test > 3, 1, 0)
+    print(roc_auc_score (y_true, y_predict))
+    return None
+
+def load_pkl_demo():
+     # 1) load data and add columns name
+    path = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"
+    column_names = ['Sample code number', 'Clump Thickness', 'Uniformity of Cell Size',
+                    'Uniformity of Cell Shape', 'Marginal Adhesion', 'Single Epithelial Cell Size',
+                    'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
+    data = pd.read_csv(path, names = column_names)
+    print(len(data), data.isnull().any())
+
+    # 2) data preprocessing: deal with missing values
+    data = data.replace(to_replace="?", value=np.nan)
+    print(len(data),data.isnull().any())
+    data.dropna(inplace = True)
+    print(len(data),data.isnull().any())
+
+    # 3) data partition
+    # pick feature columns and label column
+    x = data.iloc[:, 1:-1]
+    y = data["Class"]
+    #x_2 = data[column_names[1:10]]
+    #print(x[:10], "\n", x_2[:10])
+    x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+    # 4) feature engineer: standardize
+    transfer = StandardScaler()
+    x_train = transfer.fit_transform(x_train)
+    x_test = transfer.transform(x_test)
+
+    # 5) estimator
+    estimator = joblib.load('D:\DL_data\lr.pkl')
+
     # 6) model evaluation
     print("weights: ", estimator.coef_)
     print("b: ", estimator.intercept_)
@@ -60,6 +132,9 @@ def cancer_tumor_demo():
     print(roc_auc_score (y_true, y_predict))
     return None
 
+    return None
+
 if __name__ == "__main__":
     #titanic_demo()
     cancer_tumor_demo()
+    #load_pkl_demo()
